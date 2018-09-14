89f85f8c93af92608812d3bcba89b8464bcf0343
Alexey Yermakov <alexey@epmaki.net>
2018-09-14 08:16:12 -0600

    Calc.java Letter grade output and updated text

--- Calc.java
+++ Calc.java
@@ -7,10 +7,15 @@ class Calc{
         DecimalFormat numform = new DecimalFormat("0.00");
         System.out.println('\n' + "Quiz Grade Calculator for Mr. Madeup's Math Class!" + '\n');
 
+        //INITIALIZING VARIABLES
+
         int grade = 0, grades = 0, sum = 0, maxGrade = 0, minGrade = 100;
         double avg;
+        String letterGrade;
 
-        System.out.println("Enter quiz grades to average them (except highest and lowest score)" + '\n' + "Type a value that is not between 0 and 100 to exit" + '\n');
+        System.out.println("Enter quiz grades to average them (except highest and lowest score)" + '\n' + "Enter a value that is not between 0 and 100 (inclusive) to exit" + '\n');
+
+        //WHILE LOOP WHICH TAKES IN GRADES
 
         while(true){
             System.out.println("What grade is quiz #" + (grades + 1));
@@ -29,18 +34,37 @@ class Calc{
 
         System.out.println("");
 
+        //CALCULATING AVERAGE MINUS MAX AND MIN
+
         sum = sum - maxGrade - minGrade;
         avg = ((sum+0.0) / (grades-2));
+
+        //LETTER GRADE CALC
+
+        if(avg < 60){
+            letterGrade = "F";
+        } else if(avg >= 60 && avg < 70){
+            letterGrade = "D";
+        } else if(avg >= 70 && avg < 80){
+            letterGrade = "C";
+        } else if(avg >= 80 && avg < 90){
+            letterGrade = "B";
+        } else {
+            letterGrade = "A";
+        }
+
+        //PROGRAM END OUTPUTS
 
         if(grades == 0){
             System.out.println("No grades entered!");
         } else if(grades <= 2) {
             System.out.println("Not enough scores to calculate an average (3 or more needed)");
         } else {
-            System.out.println("grades entered: " + grades);
-            System.out.println("max: " + maxGrade);
-            System.out.println("min: " + minGrade);
+            System.out.println("Grades entered: " + grades);
+            System.out.println("Max quiz grade: " + maxGrade);
+            System.out.println("Min quiz grade: " + minGrade);
             System.out.println("Grade average: %" + numform.format(avg));
+            System.out.println("Letter grade: " + letterGrade);
         }
     }
 }
